0becd6070b3587655fad93ecc4718137112671eb
Aleem-shaikh <alim969943@gmail.com>
2026-07-21 18:26:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+alert("hello world");
+
+console.log("code is running...")
+console.log("code is running also...")
+console.log("code is looking like a wow...")
+
+var a=prompt("enter your number")
+var istrue=confirm("are you sure you want to leave this page and blast")
+
+if (istrue){
+    console.log("computer is blasting")
+}
+
+else{
+   console.log("computer is not blassting")
+}
+
+console.log("your number is " +a)
+
+document.title = "aleem sk"
+
+// document.body.style.backgroundColor="blue"
