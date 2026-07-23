d261c780c5d1bb6b3715c662dde6861c49961de2
Aleem-shaikh <alim969943@gmail.com>
2026-07-23 20:55:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-alert("hello world");
+console.log("sigma developmenr tutorial 55")
 
-console.log("code is running...")
-console.log("code is running also...")
-console.log("code is looking like a wow...")
+let a = 20;
+// a = a +1
+let b = 40;
+let c = "aleem";
+let _a ="sk";
+// var 07a ="nadeem"; starting no before name is not valeied
 
-var a=prompt("enter your number")
-var istrue=confirm("are you sure you want to leave this page and blast")
 
-if (istrue){
-    console.log("computer is blasting")
+// console.log(a + b + 8 )
+// console.log(typeof a, typeof b, typeof c,  typeof _a)
+{
+    let a =55
+    console.log(a)
 }
+console.log(a)
+// const a1 =6
+// a1= a1 +1; not allowed because a1 is constant
 
-else{
-   console.log("computer is not blassting")
-}
 
-console.log("your number is " +a)
+let x = "aleem sk";
+let y = 2;
+let z = 1.24;
+const p = true;
+let q = undefined;
+let r = null;
 
-document.title = "aleem sk"
+console.log(x, y, z, p, q, r)
+console.log(typeof x,typeof  y, typeof z,
+    typeof  p,typeof  q,typeof  r)
 
-// document.body.style.backgroundColor="blue"
+    let o = {
+        name: 'aleem',
+        "job code": 100,
+        "is_handsom":true
+    }
+
+    console.log(o)
+    o.salary = "1cr";
+    console.log(o)
+    o.salary = "2cr";
+    console.log(o)
