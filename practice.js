e0825b8a72746b50a4c38a75be389ddd310ba348
Aleem-shaikh <alim969943@gmail.com>
2026-07-24 22:26:02 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,18 @@
+document.title = "aleem sk";
+let freindname=prompt("ali bagwan")
+let dreamsalary=prompt("2lakh")
+
+
+let developerprofile = {
+    name: freindname,
+    role: "ai enabled full stack developer",
+    is_mutant: true
+}
+
+
+console.log("ai developer profile generated");
+console.log("developer profile");
+
+console.log("profile data type is: " + typeof developerprofile);
+
+alert("congratulations! " + developerprofile.name )
