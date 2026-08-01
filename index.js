d14d03a8ddc76d97a0c21ffa25b18a210c11fea7
Aleem-shaikh <alim969943@gmail.com>
2026-08-01 10:45:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-function nice(name) {
-    console.log("hey " + name + " you are nice!")
-    console.log("hey " + name + " you are good!")
-    console.log("hey " + name + " your tshirt is nice!")
-    console.log("hey " + name + " your practice is good!")
-}
+console.log("this is string tutorial")
+let a = "aleem";
 
-function sum(a, b, c = 4) {
-    // console.log(a + b)
-    return a + b + c
-}
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+// console.log(a[5]);
 
-result1 = sum(3, 5)
-result2 = sum(3, 15)
-result3 = sum(12, 5, 1)
+console.log(a.length)
 
-console.log("the sum of these number is: ", result1)
-console.log("the sum of these number is: ", result2)
-console.log("the sum of these number is: ", result3)
 
-const func1 = (x) => {
-    console.log("iam an arrow function", x)
-}
+let name = "aleem"
+let freind = "ali"
 
-func1(34);
-func1(64);
-func1(33);
+console.log(" his name is " + name + " and his friend name is " + freind)
+console.log(`his name is ${name} and his freind name is ${freind}`)
+
+let b = "muktadeermu"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1, 5))
+console.log(b.slice(1))
+
+console.log(b.replace("mu", "07"))
+console.log(b.concat(a, "arbaz", "harish"))
+
+
