64994cf0c87bb9cf42ce930f10a18450ece310da
Aleem-shaikh <alim969943@gmail.com>
2026-08-05 11:59:17 +0530

    3rd august 2026

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-console.log("this is string tutorial")
-let a = "aleem";
+let arr = [1, 2, 4, 5, 7]
+ // index  0, 1, 2, 3, 4
 
-console.log(a[0]);
-console.log(a[1]);
-console.log(a[2]);
-console.log(a[3]);
-console.log(a[4]);
-// console.log(a[5]);
+ arr[0] = 5666;
+// console.log(arr, typeof arr);
+// console.log(arr.length)
 
-console.log(a.length)
+// console.log(arr[0])
+// console.log(arr[2])
+// console.log(arr[3])
 
-
-let name = "aleem"
-let freind = "ali"
-
-console.log(" his name is " + name + " and his friend name is " + freind)
-console.log(`his name is ${name} and his freind name is ${freind}`)
-
-let b = "muktadeermu"
-console.log(b.toUpperCase())
-console.log(b.toLowerCase())
-console.log(b.length)
-console.log(b.slice(1, 5))
-console.log(b.slice(1))
-
-console.log(b.replace("mu", "07"))
-console.log(b.concat(a, "arbaz", "harish"))
+console.log(arr. toString())
+console.log(arr.join(" and "))
 
 
+
+// let numbers =[1, 2, 3, 4, 5]
+// numbers.splice(1, 2)
+// numbers.splice(1, 3)
+// numbers.splice(1, 3, 222, 333 ) 
+// (4) [1, 222, 333, 5]
