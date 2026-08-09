f7f8eb51f1d3916232dbfc5d100a621177819cc3
Aleem-shaikh <alim969943@gmail.com>
2026-08-09 11:44:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,7 @@
-console.log("sigma developmenr tutorial 55")
+console.log("hello world");
 
-let a = 20;
-// a = a +1
-let b = 40;
-let c = "aleem";
-let _a ="sk";
-// var 07a ="nadeem"; starting no before name is not valeied
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children
 
 
-// console.log(a + b + 8 )
-// console.log(typeof a, typeof b, typeof c,  typeof _a)
-{
-    let a =55
-    console.log(a)
-}
-console.log(a)
-// const a1 =6
-// a1= a1 +1; not allowed because a1 is constant
-
-
-let x = "aleem sk";
-let y = 2;
-let z = 1.24;
-const p = true;
-let q = undefined;
-let r = null;
-
-console.log(x, y, z, p, q, r)
-console.log(typeof x,typeof  y, typeof z,
-    typeof  p,typeof  q,typeof  r)
-
-    let o = {
-        name: 'aleem',
-        "job code": 100,
-        "is_handsom":true
-    }
-
-    console.log(o)
-    o.salary = "1cr";
-    console.log(o)
-    o.salary = "2cr";
-    console.log(o)
