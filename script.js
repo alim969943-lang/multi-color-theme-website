11f0a113615bef199ce087e2e8358cbba9fe41b7
Aleem-shaikh <alim969943@gmail.com>
2026-08-24 10:57:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-console.log("hello world");
+console.log("hello")
 
-document.body.firstElementChild
-document.body.firstElementChild.childNodes
-document.body.firstElementChild.children
+// let boxes = document.getElementsByClassName("box")
+// console.log (boxes)
 
+// boxes[2].style.backgroundColor = "red"
 
+// document.getElementById("redbox").style.backgroundColor ="red"
+
+// document.querySelector(".box").style.backgroundColor ="green";
+console.log(document.querySelectorAll(".box"))
+
+document.querySelectorAll(".box").forEach(e =>{
+    e.style.backgroundColor ="green";
+})
